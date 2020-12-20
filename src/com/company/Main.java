59a72207eaf27968cccef2d1980ed9018da04c43
Raccoon <andrej.mazaev@mail.ru>
2020-12-20 22:54:28 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+abstract class Animal {
+    public abstract void getName();
+}
+
+class Raccoon extends Animal {
+
+    public Object Raccoon;
+
+    @Override
+    public void getName() {
+
+    }
+}
